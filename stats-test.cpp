d1b90a66e5513647bf023b9055c9028179508f33
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2022-10-18 18:40:03 +0000

    Update stats-test.cpp

--- stats-test.cpp
+++ stats-test.cpp
@@ -24,9 +24,9 @@ TEST_CASE("average is NaN for empty array") {
     {
         printf("Yes!, this is correct \n");
     }
-    REQUIRE(isnan(computedStats.average) == 1);
-    REQUIRE(isnan(computedStats.max) == 1);
-    REQUIRE(isnan(computedStats.min) == 1);
+    REQUIRE(computedStats.average == FP_NAN);
+    REQUIRE(computedStats.max == FP_NAN);
+    REQUIRE(computedStats.min == FP_NAN);
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
