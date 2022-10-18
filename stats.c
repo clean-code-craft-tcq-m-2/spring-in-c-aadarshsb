6cce298b2c0355cdc3332a39253ba04d428bf1a0
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2022-10-18 17:32:24 +0000

    Update stats.c

--- stats.c
+++ stats.c
@@ -19,5 +19,15 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
     return s;
 }
 
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+{
+    //todo-create a separate .c and .h for fnptr alerters function( email and LED).
+    if(computedStats.max > maxThreshold)
+    {
+        //Here check the condition and call the email and led alerter
+        //and modify the email and led alert count there in fn pointer
+    }
+}
+
 int emailAlertCallCount = 0;
 int ledAlertCallCount = 0;
