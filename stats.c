67df8f35a69d5f324ccffc613ad1f5bb844193d6
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2022-10-18 16:52:45 +0000

    Update stats.c

--- stats.c
+++ stats.c
@@ -2,9 +2,19 @@
 
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    for( int iterator = 0; iterator < setlength; iterator++)
+    {
+        double TotalCount += numberset[iterator];
+        if( s.min > numberset[iterator] )
+        {
+            s.min = numberset[iterator]; 
+        }
+        if( s.max < numberset[iterator])
+        {
+            s.max = numberset[iterator]
+        }
+    }
+    s.average = (float) ( TotalCount / setlength );
     return s;
 }
 
