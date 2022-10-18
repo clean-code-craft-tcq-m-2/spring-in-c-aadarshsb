6890413a35dda2637e79d2b2c07399e7671c5b37
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2022-10-18 17:43:00 +0000

    Create alerters.c

---
+++ alerters.c
@@ -0,0 +1,14 @@
+#include "alerters.h"
+#include "stats.h"
+
+alerter_funcptr emailAlerter = emailAlerterfn;
+alerter_funcptr ledAlerter = ledAlerterfn;
+
+void emailAlerterfn()
+{
+  emailAlertCallCount++;
+}
+void ledAlerterfn()
+{
+  ledAlertCallCount++;
+}
