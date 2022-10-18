c3e5445819bddf4c6c94a482ad124dc4ee3573d1
aadarshsb <83934598+aadarshsb@users.noreply.github.com>
2022-10-18 18:23:52 +0000

    Update stats.c

--- stats.c
+++ stats.c
@@ -4,20 +4,16 @@
 struct Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
     double TotalCount;
-    unsigned char IsNaN = 0;
-    for( int iterator = 0; iterator < setlength; iterator++)
+    if(0 == setlength || 0 == numberset)
     {
-        //Check, in any case, there should not be NaN
-        //If one NaN is found, then set the average, min and max as NaN and break out of the loop
-        if(isnan(numberset[iterator]))
-        {
-            s.average = numberset[iterator];
-            s.min = numberset[iterator];
-            s.max = numberset[iterator];
-            IsNaN = 1;
-            break;
-        }
-        
+        s.average = 0.0/0.0;
+        s.min = 0.0/0.0;
+        s.max = 0.0/0.0;
+        return s;
+    }
+   
+    for( int iterator = 0; iterator < setlength; iterator++)
+    {        
         TotalCount += numberset[iterator];
         if( s.min > numberset[iterator] )
         {
@@ -28,13 +24,8 @@ struct Stats compute_statistics(const float* numberset, int setlength) {
             s.max = numberset[iterator];
         }
     }
-    if(1 == IsNaN)
-        return s;
-    else
-    {
-        s.average = (float) ( TotalCount / setlength );
-        return s;
-    }
+    s.average = (float) ( TotalCount / setlength );
+    return s;
 }
 
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
